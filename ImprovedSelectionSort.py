2f7c98ba1e09183f9639bb475d1858fc8afbc4b2
Aakhil Anvar <aakhil.official@gmail.com>
2024-12-06 21:05:08 +0000

    Create ImprovedSelectionSort.py

---
+++ ImprovedSelectionSort.py
@@ -0,0 +1,15 @@
+#Improved Selection Sort
+array = list(map(int, input("Give Array: ").split()))
+length = len(array)
+counter = 0
+print(array)
+while(counter != length-1):
+    min_value = array[counter]
+    small_ind = counter
+    for index in range(counter, length):
+        if(array[index] < min_value):
+            small_ind = index
+            min_value = array[index]
+    array[counter], array[small_ind] = array[small_ind], array[counter]
+    counter = counter + 1
+    print(array)
